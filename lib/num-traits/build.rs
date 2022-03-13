033e7b3cf4b1167c0bbb1719de9103528710fb6b
Phorena <40968207+phorena@users.noreply.github.com>
2022-03-13 11:41:18 -0700

    Add files via upload

---
+++ lib/num-traits/build.rs
@@ -0,0 +1,23 @@
+extern crate autocfg;
+
+use std::env;
+
+fn main() {
+    let ac = autocfg::new();
+
+    // If the "i128" feature is explicity requested, don't bother probing for it.
+    // It will still cause a build error if that was set improperly.
+    if env::var_os("CARGO_FEATURE_I128").is_some() || ac.probe_type("i128") {
+        autocfg::emit("has_i128");
+    }
+
+    ac.emit_expression_cfg(
+        "unsafe { 1f64.to_int_unchecked::<i32>() }",
+        "has_to_int_unchecked",
+    );
+
+    ac.emit_expression_cfg("1u32.reverse_bits()", "has_reverse_bits");
+    ac.emit_expression_cfg("1u32.trailing_ones()", "has_leading_trailing_ones");
+
+    autocfg::rerun_path("build.rs");
+}
