8a77c51bce753c5f02d6d28b9fbf0f2b8f977af2
Ashish-Abraham <ashishabraham22@gmail.com>
2022-03-28 11:48:30 +0530

    added tx,rx funcs to Advertise.rs

--- lib/client-lib/src/Advertise.rs
+++ lib/client-lib/src/Advertise.rs
@@ -2,6 +2,10 @@ use bytes::{BufMut, BytesMut};
 use custom_debug::Debug;
 use getset::{CopyGetters, Getters, MutGetters, Setters};
 use std::mem;
+use crate::{
+    Errors::ExoError,
+    ClientLib::MqttSnClient,
+};
 
 #[derive(Debug, Clone, Getters, Setters, MutGetters, CopyGetters, Default)]
 #[getset(get, set)]
@@ -29,5 +33,45 @@ impl Advertise {
     fn constraint_duration(_val: &u16) -> bool {
         //dbg!(_val);
         true
+    }
+
+    pub fn tx(gw_id: u8, duration: u16, client: &MqttSnClient) {
+        
+        let advertise = Advertise {
+            len: 5, 
+            msg_type: 0x00, 
+            gw_id: 1,
+            duration,            
+        };
+        let mut bytes_buf = BytesMut::with_capacity(advertise.len as usize);
+        dbg!(advertise.clone());
+        dbg!((bytes_buf.clone(), &advertise));
+        advertise.try_write(&mut bytes_buf);
+        dbg!(bytes_buf.clone());
+        
+        // transmit to network
+        client
+            .transmit_tx
+            .send((client.remote_addr, bytes_buf.to_owned()));
+
+    } 
+        
+
+    #[inline(always)]
+    pub fn rx(
+        buf: &[u8],
+        size: usize,
+        client: &MqttSnClient,
+    ) -> Result<(), ExoError> {
+
+        let (advertise, read_len) = Advertise::try_read(&buf, size).unwrap();
+        dbg!(advertise.clone());                                                                   
+        if read_len == size {
+            GwInfo::tx(advertise.gw_id,client);
+            Ok(())
+        } 
+        else {
+            return Err(ExoError::LenError(read_len, size));
+        }
     }
 }
