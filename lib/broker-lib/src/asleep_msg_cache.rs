8e827b41f8b338b6a6a4c52309ee32b342bbcfec
Lawrence Lu <lawrence@phorena.com>
2022-05-15 14:12:25 -0700

    more code to asleep_msg.cache.rs

--- lib/broker-lib/src/asleep_msg_cache.rs
+++ lib/broker-lib/src/asleep_msg_cache.rs
@@ -9,7 +9,7 @@ use bisetmap::BisetMap;
 use std::net::SocketAddr;
 
 lazy_static! {
-    static ref ASLEEP_MSG_CACHE: Mutex<BisetMap<(SocketAddr, MsgIdType), Publish>> =
+    static ref ASLEEP_MSG_CACHE: Mutex<BisetMap<SocketAddr, Publish>> =
         Mutex::new(BisetMap::new());
 }
 
@@ -17,12 +17,15 @@ lazy_static! {
 pub struct AsleepMsgCache {}
 
 impl AsleepMsgCache {
-    pub fn insert(key: (SocketAddr, MsgIdType), value: Publish) {
+    // Don't need vec of Publish because BisetMap allows the same key with different
+    // values. HashMap would require a Vec of Publish, one key maps to one value.
+    pub fn insert(key: SocketAddr, value: Publish) {
         let cache = ASLEEP_MSG_CACHE.lock().unwrap();
         cache.insert(key, value);
     }
 
-    pub fn delete(key: (SocketAddr, MsgIdType)) -> Vec<Publish> {
+    // returns all the Publish objects with the key.
+    pub fn delete(key: SocketAddr) -> Vec<Publish> {
         let cache = ASLEEP_MSG_CACHE.lock().unwrap();
         cache.delete(&key)
     }
@@ -41,16 +44,16 @@ fn test_asleep_cache() {
     let socket2 = "127.0.0.2:1200".parse::<SocketAddr>().unwrap();
     let bytes = BytesMut::from(&b"hello"[..]);
     let p = Publish::new(22, 22, 1, 3, bytes.clone());
-    AsleepMsgCache::insert((socket, 11), p);
+    AsleepMsgCache::insert(socket, p);
     let p = Publish::new(11, 11, 1, 3, bytes.clone());
-    AsleepMsgCache::insert((socket, 11), p);
+    AsleepMsgCache::insert(socket, p);
     let p = Publish::new(33, 33, 1, 3, bytes.clone());
-    AsleepMsgCache::insert((socket2, 11), p);
+    AsleepMsgCache::insert(socket2, p);
     let p = Publish::new(55, 55, 1, 3, bytes);
-    AsleepMsgCache::insert((socket2, 11), p);
+    AsleepMsgCache::insert(socket2, p);
 
     AsleepMsgCache::debug();
-    let msg_vec = AsleepMsgCache::delete((socket, 11));
+    let msg_vec = AsleepMsgCache::delete(socket);
     dbg!(msg_vec);
     AsleepMsgCache::debug();
 }
